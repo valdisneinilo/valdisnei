7b479e00e37e024f4c6ab43a439ec9ea53c20951
valdisneinilo <valdisneinilo@gmail.com>
2021-11-11 00:02:50 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/api/contact.js
@@ -0,0 +1,27 @@
+
+export default function enviar (req, res) {
+  let nodemailer = require('nodemailer');
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "valdisneidev@gmail.com",
+      pass: "Vv*78451200*" 
+    }
+  });
+  
+  transporter.sendMail({
+    from: "Valdisnei Nilo <valdisneidev@gmail.com>",
+    to: "valdisneidev@gmail.com",
+    subject: `Construção de ${req.body.projeto}`,
+    text: ``,
+    html: `<div>${req.body.nome}</div><div>${req.body.telefone}</div><div>${req.body.email}</div><div>${req.body.projeto}</div>`
+  }).then((response) => {res.status(200).send(response) }).catch((error)=> {res.status(500).send(error) 
+    console.log(error);
+  })
+}
+
+
+

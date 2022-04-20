db55c2468be3b1a41b8033207aba3a9d7825c965
valdisneinilo <valdisneinilo@gmail.com>
2022-04-19 21:12:20 -0300

    alteração nodemailer

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,36 +1,34 @@
 let nodemailer = require("nodemailer");
 export default function enviar(req, res) {
-
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
-    service: 'gmail',
+    service: "gmail",
     auth: {
       user: "valdisneidev@gmail.com",
-      pass: process.env.PASS
+      pass: process.env.PASS,
     },
   });
 
-  transporter.sendMail({
-    from: "valdisneidev@gmail.com",
-    replyTo: `${req.body.email}`,
-    to: "valdisneidev@gmail.com",
-    subject: `Contato site`,
-    text: ``,
-    html: `
+  transporter
+    .sendMail({
+      from: "valdisneidev@gmail.com <valdisneidev@gmail.com>",
+      replyTo: `${req.body.email}`,
+      to: "valdisneidev@gmail.com",
+      subject: `Contato site`,
+      text: ``,
+      html: `
     <div>${req.body.nome}</div>
     <div>${req.body.telefone}</div>
     <div>${req.body.email}</div>
     `,
-  })
-  .then((response) => {
-    res.status(200).send(response);
-  })
-  .catch((error) => {
-    res.status(500).send(error);
-    console.log(error);
-  });
-  
-
+    })
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+      console.log(error);
+    });
 }
